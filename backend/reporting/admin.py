bcf933cc27337a461ee5fe411bae99ef75808379
ThDat-AI <23520251@gm.uit.edu.vn>
2025-07-09 22:52:55 +0700

    Xóa model reporting

--- backend/reporting/admin.py
+++ backend/reporting/admin.py
@@ -1,12 +1,3 @@
 from django.contrib import admin
-from .models import BaoCaoMonHoc, BaoCaoHocKy
 
-@admin.register(BaoCaoMonHoc)
-class BaoCaoMonHocAdmin(admin.ModelAdmin):
-    list_display = ("IDMonHoc", "IDLopHoc", "IDHocKy", "IDNienKhoa", "SiSo", "SoLuongDat", "TiLe")
-    raw_id_fields = ("IDMonHoc", "IDLopHoc", "IDHocKy", "IDNienKhoa")
 
-@admin.register(BaoCaoHocKy)
-class BaoCaoHocKyAdmin(admin.ModelAdmin):
-    list_display = ("IDLopHoc", "IDHocKy", "IDNienKhoa", "SiSo", "SoLuongDat", "TiLe")
-    raw_id_fields = ("IDLopHoc", "IDHocKy", "IDNienKhoa")
